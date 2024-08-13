b57d867b8b150e89622613a45dfc0de5100ae921
Sergey Lukovkin <slukovkin@icloud.com>
2024-08-14 02:09:39 +0300

    update types

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { cors: true })
   const config = new DocumentBuilder()
     .setTitle('API Shop')
     .setDescription('API Shop documentation')
