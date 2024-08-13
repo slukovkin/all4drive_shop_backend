04df9bc122a4b5e2a3338d6e9808e60a28fb08b7
Sergey Lukovkin <slukovkin@icloud.com>
2024-08-13 13:15:50 +0300

    changed configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import * as process from 'node:process';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, ConfigModule.forRoot({ envFilePath: '.env' })],
+  imports: [
+    DatabaseModule,
+    UsersModule,
+    ConfigModule.forRoot(
+      { envFilePath: `.${process.env.NODE_ENV}.env` }
+    )],
   controllers: [],
   providers: []
 })
