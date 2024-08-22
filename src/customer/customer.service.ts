e1e0a204c8cf99524c729ba6614561d1940d84f5
Sergey Lukovkin <slukovkin@icloud.com>
2024-08-22 14:29:32 +0300

    created customer module

---
+++ src/customer/customer.service.ts
@@ -0,0 +1,36 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/sequelize'
+import { Customer } from './customer.model'
+import { ICustomerCreationAttrs } from './types'
+
+
+@Injectable()
+export class CustomerService {
+  constructor(
+    @InjectModel(Customer) private readonly customerRepository: typeof Customer,
+  ) {
+  }
+
+
+  async createCustomer(customerDto: ICustomerCreationAttrs) {
+    const candidate = await this.getCustomerByEmail(customerDto.email)
+    if (candidate) throw new HttpException('Клиент уже есть в БД', HttpStatus.CONFLICT)
+    return await this.customerRepository.create()
+  }
+
+  async getAllCustomers() {
+    return await this.customerRepository.findAll()
+  }
+
+  async getCustomerByEmail(email: string) {
+    return await this.customerRepository.findOne({ where: { email } })
+  }
+
+  async updateCustomerById(id: number, customer: ICustomerCreationAttrs) {
+    return await this.customerRepository.update<Customer>(customer, { where: { id } })
+  }
+
+  async deleteCustomerById(id: number) {
+    return await this.customerRepository.destroy({ where: { id: id } })
+  }
+}
---
+++ src/customer/types.ts
@@ -0,0 +1,6 @@
+export interface ICustomerCreationAttrs {
+  name: string;
+  surname: string;
+  email: string;
+  phone: string;
+}
