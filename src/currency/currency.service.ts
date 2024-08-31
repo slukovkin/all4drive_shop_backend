902e5bd260588c705d94dc16254ea918d166decb
Sergey Lukovkin <slukovkin@icloud.com>
2024-08-31 23:42:34 +0300

    updated currency module

--- src/currency/currency.service.ts
+++ src/currency/currency.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/sequelize'
 import { Currency } from './currency.model'
 import { CurrencyDto } from './dto/currency-dto'
@@ -16,7 +16,13 @@ export class CurrencyService {
     return this.currencyRepository.findOne({ where: { id } })
   }
 
+  private async getCurrencyByCode(code: string) {
+    return this.currencyRepository.findOne({ where: { code } })
+  }
+
   async create(currency: CurrencyDto) {
+    const isCurrency = this.getCurrencyByCode(currency.code)
+    if (isCurrency) throw new HttpException('Валюта с данным кодом уже есть в БД', HttpStatus.CONFLICT)
     return await this.currencyRepository.create(currency)
   }
 
